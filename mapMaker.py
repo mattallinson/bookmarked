4dc707f4d152ad6687e2385a460068510ddf9613
matt <mrallinson@gmail.com>
2021-11-21 17:14:30 +0000

    adds gitignore

--- mapMaker.py
+++ mapMaker.py
@@ -9,6 +9,8 @@ from matplotlib.pyplot import savefig
 import json
 import sys
 
+from envelope import Envelope
+
 BASEMAP = '../basemaps/ne_110m_admin_0_countries.shp'
 WORLDMAP = '../basemaps/ne_10m_admin_0_countries_lakes.shp'
 
