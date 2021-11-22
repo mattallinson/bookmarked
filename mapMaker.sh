a9ebc3ea72727bda1705300e305ab4fb8736067e
Matt Allinson <mrallinson@gmail.com>
2021-11-22 10:21:36 +0000

    Adds git backup to shellfile

--- mapMaker.sh
+++ mapMaker.sh
@@ -1,4 +1,8 @@
  #!/bin/bash
+git pull
 source activate geopandas
 cd ~/scripts/PythonScripts/map/book_project
 python -W ignore mapMaker.py
+git add *
+git commit -m "updates map with new country"
+git push
